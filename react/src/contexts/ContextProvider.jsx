254fe9692a71522f738c20caabcc7c98dac3fca6
rosamondlie <rosamond.patricia.01@gmail.com>
2025-03-28 02:50:01 +0700

    48:36

---
+++ react/src/contexts/ContextProvider.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useState } from "react";
+
+const StateContext = createContext({
+    user: null,
+    token: null,
+    setUser: () => {},
+    setToken: () => {}
+})
+
+export const ContextProvider = ({children}) => {
+    const [user, setUser] = useState({});
+    const [token, _setToken] = useState(123);
+    // disini fungsinya biar klo org udah login, bia ttp terdektsi udah login
+    const setToken = (token) => {
+        _setToken(token)
+        if (token){
+            localStorage.setItem('ACCESS_TOKEN', token);
+        }
+        else{
+            localStorage.removeItem(localStorage.getItem('ACCESS_TOKEN'));
+        }
+    }
+    return(
+        <StateContext.Provider value={{
+            user,
+            token,
+            setUser,
+            setToken
+        }}>
+            {children}
+
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext)
